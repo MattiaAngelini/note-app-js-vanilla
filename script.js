f95fcf030e74dc48b8995c175e454841413cba9a
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-16 16:23:27 +0100

    Added functions to add Notes & print on dom

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 console.log('test')
-
-
+const listNotes = document.getElementById('list-note')
+let notes = []
 
 function addNote(){
 
+    let note = {}
+    let today = new Date().toLocaleDateString();
 
-    const userInput = document.getElementById('user-note').value
+    const userInputNote = document.getElementById('user-note').value
+    const userInputTitle = document.getElementById('user-title-note').value
 
+    note['title'] = userInputTitle
+    note['text'] = userInputNote
+    note['date'] = today
+
+    notes.push(note)
+ 
+    printOnDom(notes)
+
+    console.log(notes)
     
-    console.log(userInput)
+}
+
+
+function printOnDom(arr){
+    const li = document.createElement('li')
+
+    for (let i = 0; i < arr.length; i++) {
+        li.innerHTML = arr[i].title
+        listNotes.appendChild(li)
+    }
 }
