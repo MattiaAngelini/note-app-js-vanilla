db83507d54f0657d4f3bf7c8386c110fc0172721
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-16 18:22:22 +0100

    Added notes saved in local storage persistent on DOM

--- script.js
+++ script.js
@@ -8,44 +8,53 @@
 // Aggiungi la possibilità di categorizzare le note (es. lavoro, personale).
 // Implementa una ricerca per trovare note specifiche.
 
+// localStorage.clear()
+
 const listNotes = document.getElementById('list-note')
-const notes = JSON.parse(localStorage.getItem('test'));
+
+//il programma allo start printa le note salvate nello storge SE ci sono
+const notes = JSON.parse(localStorage.getItem('test')) || [];
+printOnDom(notes)
 
 function addNote(){
-    let note = {}
-    let today = new Date().toLocaleDateString();
-    const userInputNote = document.getElementById('user-note').value
-    const userInputTitle = document.getElementById('user-title-note').value
-
-    note['id'] = notes.length
-    note['title'] = userInputTitle
-    note['text'] = userInputNote
-    note['date'] = today
-
-    notes.push(note)
-    printOnDom(notes)
-
-    localStorage.setItem('test', JSON.stringify(notes))
-
-    console.log(localStorage.getItem("test"));
-    
-}
-
-
-function printOnDom(arr){
-    const li = document.createElement('li')
-
     //BOTTONI
     const btnOpen = document.createElement('button')
     btnOpen.innerHTML = 'APRI'
     const btnDelete = document.createElement('button')
     btnDelete.innerHTML = 'ELIMINA'
 
+    //costruzione oggetto con titolo, nota e data - oggetto stampato tra 'le mie note'
+    let note = {}
+    let today = new Date().toLocaleDateString();
+    const userInputNote = document.getElementById('user-note').value
+    const userInputTitle = document.getElementById('user-title-note').value
+    note['id'] = notes.length
+    note['title'] = userInputTitle
+    note['text'] = userInputNote
+    note['date'] = today
+    const li = document.createElement('li')
+    li.innerHTML = note.title + ' ' + note.date + ' '
+    li.appendChild(btnDelete)
+    li.appendChild(btnOpen)
+    listNotes.appendChild(li)
+
+    //oggetti nota pushati in array ntoes e salvate nel local storage
+    notes.push(note)
+    localStorage.setItem('test', JSON.stringify(notes))
+}
+
+function printOnDom(arr){
+ 
     for (let i = 0; i < arr.length; i++) {
+        //BOTTONI
+        const btnOpen = document.createElement('button')
+        btnOpen.innerHTML = 'APRI'
+        const btnDelete = document.createElement('button')
+        btnDelete.innerHTML = 'ELIMINA'
+        const li = document.createElement('li')
         li.innerHTML = arr[i].title + ' ' +  arr[i].date + ' '
         li.appendChild(btnDelete)
         li.appendChild(btnOpen)
         listNotes.appendChild(li)
     }
 }
-
