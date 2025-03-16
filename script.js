78c2f2b79b069a384075de0629a30f3007527693
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-16 20:26:54 +0100

    Exercise completed

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 // Descrizione: Crea un'applicazione per prendere appunti.
-
 // Funzionalità:
 // Aggiungi, leggi e elimina note.
 // Salva le note nel localStorage per renderle persistenti.
 
-// Funzionalità aggiuntive:
-// Aggiungi la possibilità di categorizzare le note (es. lavoro, personale).
-// Implementa una ricerca per trovare note specifiche.
-
 // localStorage.clear()
 
 const listNotes = document.getElementById('list-note')
+const modal = document.getElementById('ms-modal')
+const modalTitle = document.getElementById('modal-title')
+const modalText = document.getElementById('modal-text')
 
 //il programma allo start printa le note salvate nello storge SE ci sono
 const notes = JSON.parse(localStorage.getItem('test')) || [];
 printOnDom(notes)
 
+//funzione che trasforma nota in oggetto, la stampa con due bottoni apri o elimina, 
+// pusha obj in array e salva array con notes nel local storage
 function addNote(){
-
     //BOTTONI
     const btnOpen = document.createElement('button')
+    btnOpen.classList.add('btn','btn-primary', 'm-1')
     btnOpen.innerHTML = 'APRI'
     const btnDelete = document.createElement('button')
+    btnDelete.classList.add('btn','btn-danger')
     btnDelete.innerHTML = 'ELIMINA'
 
     btnDelete.addEventListener('click', function(){
         notes.splice(note.id, 1);
         localStorage.setItem('test', JSON.stringify(notes));
+        location.reload();
     })
-
+    btnOpen.addEventListener('click', function(){
+        modal.classList.remove('d-none')
+        modalTitle.innerHTML = note.title
+        modalText.innerHTML = note.text
+    })
+        
     //costruzione oggetto con titolo, nota e data - oggetto stampato tra 'le mie note'
     let note = {}
     let today = new Date().toLocaleDateString();
@@ -39,7 +46,7 @@ function addNote(){
     note['text'] = userInputNote
     note['date'] = today
     const li = document.createElement('li')
-    li.innerHTML = note.title + ' ' + note.date + ' '
+    li.innerHTML = note.title + ' - ' + note.date + ' - '
     li.appendChild(btnDelete)
     li.appendChild(btnOpen)
     listNotes.appendChild(li)
@@ -49,25 +56,38 @@ function addNote(){
     localStorage.setItem('test', JSON.stringify(notes))
 }
 
+//funzione che stampa nel dom la lista dell note salvate nel local storage,
+//con bottoni per eliminare nota o visualizzarla nella modale
 function printOnDom(arr){
- 
+
     for (let i = 0; i < arr.length; i++) {
         //BOTTONI
         const btnOpen = document.createElement('button')
+        btnOpen.classList.add('btn','btn-primary', 'm-1')
         btnOpen.innerHTML = 'APRI'
         const btnDelete = document.createElement('button')
         btnDelete.innerHTML = 'ELIMINA'
-
+        btnDelete.classList.add('btn','btn-danger')
         btnDelete.addEventListener('click', function(){
             notes.splice(i, 1);
             localStorage.setItem('test', JSON.stringify(notes));
+            location.reload();
         })
-        
+
+        btnOpen.addEventListener('click', function(){
+        modal.classList.remove('d-none')
+        modalTitle.innerHTML = arr[i].title
+        modalText.innerHTML = arr[i].text
+        })
         const li = document.createElement('li')
-        li.innerHTML = arr[i].title + ' ' +  arr[i].date + ' '
+        li.innerHTML = arr[i].title + ' - ' +  arr[i].date + ' - '
         li.appendChild(btnDelete)
         li.appendChild(btnOpen)
         listNotes.appendChild(li)
     }
     console.log(notes)
+}
+
+function closeModal(){
+    modal.classList.add('d-none')
 }
