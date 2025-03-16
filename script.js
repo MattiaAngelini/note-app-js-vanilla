1392751ba43ca4e491ba3925bb286694983259c0
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-16 17:04:02 +0100

    Added notes on localStorage

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-console.log('test')
+// Descrizione: Crea un'applicazione per prendere appunti.
+
+// Funzionalità:
+// Aggiungi, leggi e elimina note.
+// Salva le note nel localStorage per renderle persistenti.
+
+// Funzionalità aggiuntive:
+// Aggiungi la possibilità di categorizzare le note (es. lavoro, personale).
+// Implementa una ricerca per trovare note specifiche.
+
 const listNotes = document.getElementById('list-note')
-let notes = []
+const notes = JSON.parse(localStorage.getItem('test'));
 
 function addNote(){
-
     let note = {}
     let today = new Date().toLocaleDateString();
-
     const userInputNote = document.getElementById('user-note').value
     const userInputTitle = document.getElementById('user-title-note').value
 
+    note['id'] = notes.length
     note['title'] = userInputTitle
     note['text'] = userInputNote
     note['date'] = today
 
     notes.push(note)
- 
     printOnDom(notes)
 
-    console.log(notes)
+    localStorage.setItem('test', JSON.stringify(notes))
+
+    console.log(localStorage.getItem("test"));
     
 }
 
@@ -26,8 +35,17 @@ function addNote(){
 function printOnDom(arr){
     const li = document.createElement('li')
 
+    //BOTTONI
+    const btnOpen = document.createElement('button')
+    btnOpen.innerHTML = 'APRI'
+    const btnDelete = document.createElement('button')
+    btnDelete.innerHTML = 'ELIMINA'
+
     for (let i = 0; i < arr.length; i++) {
-        li.innerHTML = arr[i].title
+        li.innerHTML = arr[i].title + ' ' +  arr[i].date + ' '
+        li.appendChild(btnDelete)
+        li.appendChild(btnOpen)
         listNotes.appendChild(li)
     }
 }
+
